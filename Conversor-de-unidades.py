10c4f9a825c72756792237facc49d90302c59ec2
ZeusBiel <127859799+ZeusBiel@users.noreply.github.com>
2023-03-21 09:23:48 -0300

    Versão 1.0

--- Conversor-de-unidades.py
+++ Conversor-de-unidades.py
@@ -1,25 +1,30 @@
-from unittest import result
-
-
-valor = float(input("Insira o valor: "))
-unidade_origem = input("Insira a origem ")
-unidade_destino = input("Insira o destino: ")
 
 def converter_unidade(valor, unidade_origem, unidade_destino):
     if unidade_origem == 'km' and unidade_destino == 'mi':
         resultado = valor / 1.60934
+        print(f"{resultado}mi")
     elif unidade_origem == 'mi' and unidade_destino == 'km':
         resultado = valor * 1.60934
+        print(f"{resultado}km")
     elif unidade_origem == 'kg' and unidade_destino == 'lb':
         resultado = valor * 2.20462
+        print(f"{resultado}lb")
     elif unidade_origem == 'lb' and unidade_destino == 'kg':
         resultado = valor / 2.20462
+        print(f"{resultado}kg")
     elif unidade_origem == 'm' and unidade_destino == 'ft':
         resultado = valor * 3.28084
+        print(f"{resultado}ft")
     elif unidade_origem == 'ft' and unidade_destino == 'm':
         resultado = valor / 3.28084
+        print(f"{resultado}m")
     else:
         print("Unidades de medida não suportadas.")
         return None
     return resultado
 
+
+valor = float(input("Insira o valor: "))
+unidade_origem = input("Insira a origem: ")
+unidade_destino = input("Insira o destino: ")
+converter_unidade(valor, unidade_origem, unidade_destino)
