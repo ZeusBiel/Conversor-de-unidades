790566c94e53ea3142cd4aff730e6e69601be69d
ZeusBiel <127859799+ZeusBiel@users.noreply.github.com>
2023-03-21 09:38:11 -0300

    Versão 1.0

--- Conversor-de-unidades.py
+++ Conversor-de-unidades.py
@@ -2,22 +2,22 @@
 def converter_unidade(valor, unidade_origem, unidade_destino):
     if unidade_origem == 'km' and unidade_destino == 'mi':
         resultado = valor / 1.60934
-        print(f"{resultado}mi")
+        print(f"{resultado} mi")
     elif unidade_origem == 'mi' and unidade_destino == 'km':
         resultado = valor * 1.60934
-        print(f"{resultado}km")
+        print(f"{resultado} km")
     elif unidade_origem == 'kg' and unidade_destino == 'lb':
         resultado = valor * 2.20462
-        print(f"{resultado}lb")
+        print(f"{resultado} lb")
     elif unidade_origem == 'lb' and unidade_destino == 'kg':
         resultado = valor / 2.20462
-        print(f"{resultado}kg")
+        print(f"{resultado} kg")
     elif unidade_origem == 'm' and unidade_destino == 'ft':
         resultado = valor * 3.28084
-        print(f"{resultado}ft")
+        print(f"{resultado} ft")
     elif unidade_origem == 'ft' and unidade_destino == 'm':
         resultado = valor / 3.28084
-        print(f"{resultado}m")
+        print(f"{resultado} m")
     else:
         print("Unidades de medida não suportadas.")
         return None
